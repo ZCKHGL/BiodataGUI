740c4e4c5cdc2cfa4fc09d1c0127daa5c3076073
ZCKHGL <164823089+ZCKHGL@users.noreply.github.com>
2024-11-04 10:14:35 +0700

    Add files via upload

---
+++ BiodataPert3/src/main/java/zckhgl/biodatapert3/BiodataPert3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zckhgl.biodatapert3;
+
+/**
+ *
+ * @author excee
+ */
+public class BiodataPert3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
